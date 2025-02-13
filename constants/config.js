e73b8052530f78008de86530a7c2ed4d1f619f7b
dinhbaokhanh <khanhdbao@gmail.com>
2025-02-13 07:25:43 +0700

    Finished Server

--- constants/config.js
+++ constants/config.js
@@ -2,6 +2,7 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
+    'https://chatbox-server-l5wn.onrender.com',
     process.env.CLIENT_URL,
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
